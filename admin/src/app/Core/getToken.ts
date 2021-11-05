2022fe8bad1e8fcefcd5ef873d472b9789160cf9
svyuhher qawsedrf <tanjamaltzevatanjabor@mail.ru>
2021-11-05 18:34:08 +0300

    .gitignore is now working, hopefully

--- admin/src/app/Core/getToken.ts
+++
@@ -1,6 +0,0 @@
-import { ACCESS_TOKEN_KEY } from '../auth.service';
-
-
-export function getToken(): string | null {
-  return localStorage.getItem(ACCESS_TOKEN_KEY);
-}
--- iTechArt.Hotels.Database/Accounts.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Accounts] (
-    [Id] INT IDENTITY (1, 1) NOT NULL,
-    [Email] VARCHAR (125) NOT NULL,
-    [Password] VARCHAR (20) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
