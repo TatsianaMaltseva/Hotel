dfd32ffdc311263ecaf5f5cebbb784e04581d77f
svyuhher qawsedrf <tanjamaltzevatanjabor@mail.ru>
2021-11-08 13:11:47 +0300

    ewgistration dialog appears

--- admin/src/app/Components/registration/registration.component.ts
+++ admin/src/app/Components/registration/registration.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { AuthService } from 'src/app/auth.service';
 
 @Component({
@@ -8,11 +10,37 @@ import { AuthService } from 'src/app/auth.service';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent{
+  public registrationForm: FormGroup;
+
 
   public constructor(
     private readonly authService: AuthService,
-    private readonly matDialogRef: MatDialogRef<RegistrationComponent>
-  ) { }
+    private readonly matDialogRef: MatDialogRef<RegistrationComponent>,
+    private readonly formBuilder: FormBuilder) {
+    this.registrationForm = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', Validators.required]
+    });
+  }
+
+  public get email(): AbstractControl | null {
+    return this.registrationForm.get('email');
+  }
+
+  public get password(): AbstractControl | null {
+    return this.registrationForm.get('password');
+  }
+
+  public get confirmPassword(): AbstractControl | null {
+    return this.registrationForm.get('congirmPassword');
+  }
 
   public closeRegistrationDialog(): void {
     this.matDialogRef.close();
