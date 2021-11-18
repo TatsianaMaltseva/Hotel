b48918b237bfc7bcb8e9534234e9ce679b33b866
svyuhher qawsedrf <tanjamaltzevatanjabor@mail.ru>
2021-11-18 18:01:30 +0300

    rename accounts

---
+++ api/Controllers/AccountsController.cs
@@ -0,0 +1,54 @@
+﻿using iTechArt.Hotels.Api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iTechArt.Hotels.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : Controller
+    {
+        private readonly HotelsDatabaseContext _hotelsDb;
+
+        public AccountsController(HotelsDatabaseContext hotelsDb)
+        {
+            _hotelsDb = hotelsDb;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> CreateAccount([FromBody] Login request)
+        {
+            if (!CheckIfEmailUnique(request.Email))
+            {
+                return BadRequest("User is already registered with this email");
+            }
+            var user = new Account
+            {
+                Email = request.Email,
+                Password = request.Password
+            };
+            _hotelsDb.Add(user);
+            await _hotelsDb.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetAccountEmailAsync), new { id = user.Id }, null);
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetAccountEmailAsync([FromRoute] int Id)
+        {
+            string email = await _hotelsDb.Accounts
+                .Where(account => account.Id == Id)
+                .Select(account => account.Email)
+                .SingleAsync();
+            return Ok(email);
+        }
+
+        private bool CheckIfEmailUnique(string email) =>
+            !_hotelsDb.Accounts.Any(u => u.Email == email);
+    }
+}
